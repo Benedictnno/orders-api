559a2b7d2bf2b9b6257ebc510e2780d2033332a4
Benedictnno <benedictnnaoma0@gmail.com>
2026-05-21 13:36:56 +0100

    added order structure

--- application/app.go
+++ application/app.go
@@ -1,31 +1,70 @@
-package application 
+package application
 
 import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{
+			Addr: "localhost:6379",
+		}),
 	}
 	return app
-	}
+}
 
-	func (a *App) Start(ctx context.Context) error {
+func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("Error starting server: %w", err)
+		return fmt.Errorf("Error connecting to Redis: %w", err)
+	}
+
+	defer func() {
+		err := a.rdb.Close()
+		if err != nil {
+			fmt.Println("Error closing Redis connection:", err)
+		}
+	}()
+
+	fmt.Println("Connected to Redis successfully")
+	ch := make(chan error, 1)
+	go func() {
+
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("Error starting server: %w", err)
+		}
+		close(ch)
+
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		err := server.Shutdown(timeout)
+		if err != nil {
+			return fmt.Errorf("Error shutting down server: %w", err)
+		}
 	}
 	return nil
 
-	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Benedictnno/orders-api
 
 go 1.25.5
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/redis/go-redis/v9 v9.19.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
